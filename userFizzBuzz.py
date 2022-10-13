9c3367b0a7aaa1ab5744c8a8d544dd68ae78487d
Subzy132 <subhaanh00@gmail.com>
2022-10-13 15:52:50 +0100

    added functions

--- userFizzBuzz.py
+++ userFizzBuzz.py
@@ -1,13 +1,17 @@
-
 a = int(input("Pick a number you would like to be Fizz: "))
 b = int(input("Pick a number you would like to be Buzz: "))
 
-for x in range(1, 100):
-    if  x % a == 0 and x % b == 0:
-        print("FizzBuzz")
-    elif x % b == 0:
-        print("Buzz")
-    elif x % a == 0:
-        print("Fizz")
-    else:
-        print(x)
+
+def fizzbuzz(num1, num2):
+
+    for x in range(1, 100):
+        if x % a == 0 and x % b == 0:
+            print("FizzBuzz")
+        elif x % num2 == 0:
+            print("Buzz")
+        elif x % num1 == 0:
+            print("Fizz")
+        else:
+            print(x)
+
+fizzbuzz(a, b)
